32d5565c0053db899b8ee49a5a675dac40e10dbc
Suresh Subramaniam <suresh@gamemaki.com>
2012-08-07 13:36:05 +0530

    Added getAllLoans() method

--- src/com/suresh/whereismycash/db/DbHelper.java
+++ src/com/suresh/whereismycash/db/DbHelper.java
@@ -1,15 +1,13 @@
 package com.suresh.whereismycash.db;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
 public class DbHelper extends SQLiteOpenHelper {
 	
-	/**
-	 * Log tag
-	 */
 	private static final String TAG = "DbHelper";
 	
     /**
@@ -20,8 +18,19 @@ public class DbHelper extends SQLiteOpenHelper {
     private static final int DATABASE_VERSION = 2;
     private static final String DATABASE_CREATE =
 	    "create table " + DATABASE_TABLE + " (_id integer primary key autoincrement, "
-	    + "name varchar(100) not null, amount float not null, paid float not null, " +
+	    + "name varchar(100) not null, amount float not null, paid float not null, reason text, " +
 	    "receive boolean not null, created_at timestamp not null default current_timestamp);";
+    
+    /**
+     * SQL Column Keys
+     */
+    private static final String KEY_ID = "_id";
+    private static final String KEY_NAME = "name";
+    private static final String KEY_AMOUNT = "amount";
+    private static final String KEY_PAID = "paid";
+    private static final String KEY_RECEIVE = "receive";
+    private static final String KEY_REASON = "reason";
+    private static final String KEY_CREATED_AT = "created_at";
 
 	public DbHelper(Context context) {
 		super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -38,6 +47,13 @@ public class DbHelper extends SQLiteOpenHelper {
                 + newVersion + ", which will destroy all old data");
         db.execSQL("DROP TABLE IF EXISTS " + DATABASE_TABLE);
         onCreate(db);
+	}
+	
+	public Cursor getAllLoans(boolean receive) {
+		SQLiteDatabase db = getWritableDatabase();
+		String[] columns = {KEY_ID, KEY_NAME, KEY_AMOUNT, KEY_PAID};
+		Cursor c = db.query(DATABASE_TABLE, columns, KEY_RECEIVE + " = " + receive, null, null, null, KEY_CREATED_AT + " desc");
+		return c;
 	}
 
 }
