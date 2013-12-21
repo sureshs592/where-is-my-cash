2e2f29b15733364ded084c8b740930802096e144
Suresh Subramaniam <sureshs.592@gmail.com>
2013-12-21 21:23:21 +0530

    Storing versionCode in SharedPrefs file and displaying update info dialog to users

--- src/com/suresh/whereismycash/MainActivity.java
+++ src/com/suresh/whereismycash/MainActivity.java
@@ -1,8 +1,15 @@
 package com.suresh.whereismycash;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.AlertDialog.Builder;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.widget.CursorAdapter;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -21,6 +28,7 @@ public class MainActivity extends SherlockListActivity implements OnClickListene
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        displayOneTimeInfoDialog();
     }
     
     public void initializeList() {
@@ -77,6 +85,27 @@ public class MainActivity extends SherlockListActivity implements OnClickListene
 		float netSum = dbHelper.getNetSum();
 		TextView tvNetAmount = (TextView) findViewById(R.id.tvNetAmount);
 		DbHelper.setTextandColor(this, tvNetAmount, netSum);
+	}
+	
+	public void displayOneTimeInfoDialog() {
+		try {
+			SharedPreferences info = getSharedPreferences("info", MODE_PRIVATE);
+			int storedVersionCode = info.getInt("versionCode", -1);
+			int currentVersionCode = getPackageManager().getPackageInfo(getPackageName(), 0).versionCode;
+			
+			if (storedVersionCode < currentVersionCode && MiscUtil.phoneSupportsSwipe()) { //Display information dialog
+				AlertDialog.Builder builder = new Builder(this);
+				builder.setTitle(R.string.update_dialog_title);
+				builder.setMessage(R.string.update_info);
+				builder.setNeutralButton(R.string.dialog_ok, null);
+				AlertDialog dialog = builder.create();
+				dialog.show();
+			}
+			
+			Editor editor = info.edit();
+			editor.putInt("versionCode", currentVersionCode);
+			editor.commit();
+		} catch (NameNotFoundException ignore) { }
 	}
     
 }
